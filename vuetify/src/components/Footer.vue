01d5fc3d24b15dd31c76fdc4b19222e84935276b
Herman Dempere <nerucius@gmail.com>
2019-03-10 20:58:26 +0100

    Vuetify Footer Component and router cleanup

---
+++ vuetify/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card flat>
+    <v-card-text>
+      <h3>Footer Content</h3>
+
+      <v-layout row>
+        <v-flex xs4>
+          <a href="#">
+            Link one
+          </a>
+          <br>
+          <a href="#">
+            Link two
+          </a>
+        </v-flex>
+
+        <v-spacer />
+
+        <v-flex xs4>
+          &copy; Some Company LTD 2019
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>

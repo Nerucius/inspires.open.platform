6a69c842d35ebd3a4e43492b7d0ca14e79a0246c
Nerucius <nerucius@gmail.com>
2019-03-13 10:25:16 +0100

    Vue smarter Project Store

--- vuetify/src/store/Project.js
+++ vuetify/src/store/Project.js
@@ -19,24 +19,27 @@ export default {
   },
 
   actions: {
-    load: async function (context, ids) {
-      if (!ids){
-        // No ids provided, just get list of all
-        let items = (await ProjectResource.get({ordering: "-modified_at"})).body.results
-        context.commit("SET", items)
-      }else{
+    load: async function (context, payload={}) {
+      if (Array.isArray(payload)){
         // Ids provided, get detailed information on given pids
-        let items = (await Promise.all(
-          ids.map(id => ProjectResource.get({id}) )
-        ))
+        let ids = payload
+        let items = await Promise.all(ids.map(id => ProjectResource.get({id})))
         items = items.map(i => i.body)
         items.map(item => context.commit("SET_MAP", {id:item.id, item}))
+
+      }else{
+        // No ids provided, just get list of all
+        let query = {ordering: "-modified_at", ...(payload.params || {})}
+        let items = (await ProjectResource.get(query)).body.results
+        context.commit("SET", items)
       }
     },
 
     create: async function (context, object){
-      let result = (await ProjectResource.save(object))
-      context.dispatch("load")
+      let result = await ProjectResource.save(object)
+      let project = result.body
+      await context.dispatch("load", [project.id])
+      return project
     },
 
     update: async function (context, object){

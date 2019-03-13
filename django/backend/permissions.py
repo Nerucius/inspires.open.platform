5cdb18b54a8963a1d91dd854a6e9761f42a5cdca
Nerucius <nerucius@gmail.com>
2019-03-13 10:24:15 +0100

    Django AnonReadonly Permission

--- django/backend/permissions.py
+++ django/backend/permissions.py
@@ -11,17 +11,18 @@ class CustomPermissionSet(permissions.DjangoModelPermissions):
     """
 
     CREATE_METHOD = "POST"
+    authenticated_users_only = False
 
     # Custom permission map with VIEW permission added
-    perms_map = {
-        "GET": ["%(app_label)s.view_%(model_name)s"],
-        "OPTIONS": ["%(app_label)s.view_%(model_name)s"],
-        "HEAD": ["%(app_label)s.view_%(model_name)s"],
-        "POST": ["%(app_label)s.add_%(model_name)s"],
-        "PUT": ["%(app_label)s.change_%(model_name)s"],
-        "PATCH": ["%(app_label)s.change_%(model_name)s"],
-        "DELETE": ["%(app_label)s.delete_%(model_name)s"],
-    }
+    # perms_map = {
+    #     "GET": ["%(app_label)s.view_%(model_name)s"],
+    #     "OPTIONS": ["%(app_label)s.view_%(model_name)s"],
+    #     "HEAD": ["%(app_label)s.view_%(model_name)s"],
+    #     "POST": ["%(app_label)s.add_%(model_name)s"],
+    #     "PUT": ["%(app_label)s.change_%(model_name)s"],
+    #     "PATCH": ["%(app_label)s.change_%(model_name)s"],
+    #     "DELETE": ["%(app_label)s.delete_%(model_name)s"],
+    # }
 
     def has_permission(self, request, view):
         """ Separate into read VS modify. """
@@ -30,18 +31,12 @@ class CustomPermissionSet(permissions.DjangoModelPermissions):
         if not hasattr(view, "get_queryset"):
             return True
 
-        if not request.user or not request.user.is_authenticated:
-            return False
-
         if request.method in permissions.SAFE_METHODS:
             return self.has_read_permission(request, view)
         return self.has_write_permission(request, view)
 
     def has_object_permission(self, request, view, obj):
         """ Separate into read VS modify. """
-
-        if not request.user or not request.user.is_authenticated:
-            return False
 
         if request.method in permissions.SAFE_METHODS:
             return self.has_object_read_permission(request, view, obj)

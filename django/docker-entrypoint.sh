76fa97d7802131c8346e9a7683319b45befb2600
Nerucius <nerucius@gmail.com>
2019-03-19 12:24:04 +0100

    Django fixed loaddata command

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -7,10 +7,10 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 # Load necessary fixtures
 echo "Loading Fixtures..."
-python loaddata auth-group
-python loaddata auth-user
-python loaddata knowledgearea
-python loaddata participationrole
+python manage.py loaddata auth-group
+python manage.py loaddata auth-user
+python manage.py loaddata knowledgearea
+python manage.py loaddata participationrole
 # echo "from backend.models import User; \
 # User.objects.create_superuser('admin', 'admin@localhost', '123456')" \
 # | python manage.py shell

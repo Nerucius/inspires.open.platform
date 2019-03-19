c250c348d5245e20d0e58bb84de98855cec75d14
Nerucius <nerucius@gmail.com>
2019-03-19 13:51:27 +0100

    Vue disabled clean

--- vuetify/docker-entrypoint.sh
+++ vuetify/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Cleaning old files, skipping static/"
-find ./dist/ -maxdepth 1 | grep -v "^./dist/$" | grep -v "static" | xargs rm -r
+# find ./dist/ -maxdepth 1 | grep -v "^./dist/$" | grep -v "static" | xargs rm -r
 
 echo "Begin Vue Build"
 npm run build:docker

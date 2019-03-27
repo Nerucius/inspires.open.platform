12271dc281c43093bec7880f957fbf74db915f96
Nerucius <nerucius@gmail.com>
2019-03-27 09:32:35 +0100

    Vue profile menu dropdown

--- vuetify/src/components/toolbar/LoginLogoutButton.vue
+++ vuetify/src/components/toolbar/LoginLogoutButton.vue
@@ -6,29 +6,59 @@
     </v-icon>
   </v-btn>
 
-  <v-btn v-else flat @click="logout()">
+  <!-- <v-btn v-else flat @click="logout()">
     {{ $t('actions.logout') }}
     <v-icon right>
       exit_to_app
     </v-icon>
-  </v-btn>
+  </v-btn>-->
+
+  <v-menu v-else offset-y>
+    <v-btn slot="activator" flat>
+      {{ user.first_name }}
+      <v-icon right>
+        expand_more
+      </v-icon>
+    </v-btn>
+    <v-list>
+      <v-list-tile :to="{name:'account'}">
+        <v-list-tile-title>
+          Profile
+        </v-list-tile-title>
+      </v-list-tile>
+
+      <v-divider />
+
+      <v-list-tile @click="logout()">
+        <v-list-tile-title>
+          Log out
+          <v-icon right>
+            exit_to_app
+          </v-icon>
+        </v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
 </template>
 
 
 <script>
 export default {
-  name:"LoginLogoutButton",
+  name: "LoginLogoutButton",
 
   computed: {
     isLoggedIn() {
       return this.$store.getters["user/isLoggedIn"];
     },
+    user() {
+      return this.$store.getters["user/current"];
+    }
   },
 
-  methods:{
-    logout(){
-      this.$store.dispatch("user/logout")
+  methods: {
+    logout() {
+      this.$store.dispatch("user/logout");
     }
   }
-}
+};
 </script>

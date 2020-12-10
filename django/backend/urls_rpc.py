6b1da311b888ab467bf4219403fcfc28ded3f584
Herman Dempere <herman.dempere@emark.com>
2020-12-10 23:15:53 +0100

    Django RPC to update all projects to eval 2

--- django/backend/urls_rpc.py
+++ django/backend/urls_rpc.py
@@ -4,4 +4,5 @@ from backend import views_rpc as views
 
 urlpatterns = [
     path("project/<int:project>/invite", views.InviteProjectParticipant.as_view()),
+    path("projects/eval/update", views.UpdateAllProjectsEvaluation.as_view()),
 ]
--- django/backend/views_rpc.py
+++ django/backend/views_rpc.py
@@ -9,6 +9,7 @@ from django.http.response import (
     HttpResponseForbidden,
     HttpResponseBadRequest,
 )
+from django.db import transaction
 
 from rest_framework.authtoken.models import Token
 
@@ -18,10 +19,12 @@ from backend.serializers import SimpleUserSerializer
 
 from backend.models import User, Group
 
+
 def json_response(data, status=200):
     return HttpResponse(
         json.dumps(data), status=status, content_type="application/json"
     )
+
 
 def json_response_error(exception, status=400):
     data = {"detail": str(exception)}
@@ -29,30 +32,36 @@ def json_response_error(exception, status=400):
         json.dumps(data), status=status, content_type="application/json"
     )
 
-class InviteProjectParticipant(View):
 
+class InviteProjectParticipant(View):
     def post(self, request, project, *args, **kwargs):
         project = Project.objects.get(pk=project)
         try:
             _authenticate_request(request, project)
             if not request.user.is_superuser:
-                assert project.can_write(request.user), "Not authorized to modify this Project."
+                assert project.can_write(
+                    request.user
+                ), "Not authorized to modify this Project."
                 assert project.structure != None, "Project not linked to any structure."
                 assert project.structure.validation != None, "Structure not validated."
-                assert project.structure.validation.is_approved, "Structure not validated."
-                assert project.collaboration.is_approved, "Your Structure has not validated this Project."
+                assert (
+                    project.structure.validation.is_approved
+                ), "Structure not validated."
+                assert (
+                    project.collaboration.is_approved
+                ), "Your Structure has not validated this Project."
         except Exception as e:
             return json_response_error(e)
 
         request_data = json.loads(request.body)
 
         # Create a fictitious email if none is sent
-        if 'email' in request_data and request_data['email'] != '':
-            user_username = request_data['email']
-            user_email = request_data['email']
+        if "email" in request_data and request_data["email"] != "":
+            user_username = request_data["email"]
+            user_email = request_data["email"]
         else:
             user_username = str(uuid.uuid4())
-            user_email = user_username + '@app.inspiresproject.com'
+            user_email = user_username + "@app.inspiresproject.com"
 
         # Check for existing users
         if User.objects.filter(email=user_email).exists():
@@ -61,12 +70,13 @@ class InviteProjectParticipant(View):
         # Create user including empty field defaults
         newUser = User(
             username=user_username,
-            first_name=request_data['first_name'],
-            last_name=request_data['last_name'],
+            first_name=request_data["first_name"],
+            last_name=request_data["last_name"],
             email=user_email,
-            hide_realname='hide_realname' in request_data and request_data['hide_realname'],
-            gender=request_data['gender'] if 'gender' in request else '',
-            institution=request_data['institution'] if 'institution' in request else '',
+            hide_realname="hide_realname" in request_data
+            and request_data["hide_realname"],
+            gender=request_data["gender"] if "gender" in request else "",
+            institution=request_data["institution"] if "institution" in request else "",
         )
         try:
             newUser.save()
@@ -85,7 +95,7 @@ class InviteProjectParticipant(View):
         participation = Participation(
             user=newUser,
             project=project,
-            role=ParticipationRole.objects.get(pk=request_data['role'])
+            role=ParticipationRole.objects.get(pk=request_data["role"]),
         )
 
         try:
@@ -94,11 +104,56 @@ class InviteProjectParticipant(View):
             newUser.delete()
             return json_response_error("Could not link User to Project: " + str(e))
 
+        return json_response(
+            {"status": "OK", "user_id": newUser.pk, "eval_token": newUser.eval_token}
+        )
+
+
+CURRENT_EVAL_VERSION = 1
+LATEST_EVAL_VERSION = 2
+
+
+class UpdateAllProjectsEvaluation(View):
+    """ API to update all existing Projects without evaluation data to the latest version """
+
+    def post(self, request, *args, **kwargs):
+        _authenticate_request(request)
+        if not request.user.is_superuser:
+            return json_response_error("User must be super-user to trigger update")
+
+        request_data = json.loads(request.body)
+
+        projects = Project.objects.filter(eval_version__lt=LATEST_EVAL_VERSION)
+        projects_to_update = []
+        projects_to_not_update = []
+
+        for project in projects:
+            project_has_evals = Evaluation.objects.filter(
+                phase__project=project
+            ).exists()
+
+            if not project_has_evals:
+                projects_to_update += [project]
+            else:
+                projects_to_not_update += [project]
+
+        if "execute" in request_data and request_data["execute"] == True:
+            status = "OK"
+            try:
+                with transaction.atomic():
+                    for project in projects_to_update:
+                        project.eval_version = LATEST_EVAL_VERSION
+                        project.save()
+            except Exception as e:
+                return json_response_error(e)
+
+        else:
+            status = "DRY_RUN"
 
         return json_response(
             {
-                "status": "OK",
-                "user_id": newUser.pk,
-                "eval_token": newUser.eval_token
+                "status": status,
+                "projects_to_update": len(projects_to_update),
+                "projects_to_not_update": len(projects_to_not_update),
             }
         )
